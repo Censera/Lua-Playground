8a3c50e4a1566e550731ec921c0669f941934df3
Censera <Censera@proton.me>
2025-12-07 00:58:11 +0100

    Arithmetics

---
+++ arithmetics/main.lua
@@ -0,0 +1,14 @@
+x = 8
+y = 4
+
+add = x + y
+sub = x - y
+mul = x * y
+div = x / y
+mod = x % (y + 1)
+
+io.write(x, " + ", y, " = ", add, "\n")
+io.write(x, " - ", y, " = ", sub, "\n")
+io.write(x, " * ", y, " = ", mul, "\n")
+io.write(x, " / ", y, " = ", div, "\n")
+io.write(x, " % ", y+1, " = ", mod, "\n")
